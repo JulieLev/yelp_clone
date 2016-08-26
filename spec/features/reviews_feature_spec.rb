1f07a8325d9c1e41356ddb83e15da4f6419eeb0f
JulieLev <jkgee1@yahoo.co.uk>
2016-08-26 17:21:38 +0100

    user can only delete his own reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature 'reviewing' do
-  before { Restaurant.create name: 'KFC' }
+  before { sign_up_and_add_restaurant }
+  let (:kfc) { Restaurant.find_by_name('KFC') }
 
   scenario 'allows users to leave a review using a form' do
     visit '/restaurants'
@@ -11,7 +12,32 @@ feature 'reviewing' do
     click_button 'Leave Review'
 
     expect(current_path).to eq '/restaurants'
+    click_link 'KFC'
     expect(page).to have_content('so so')
+  end
+
+
+  scenario 'allows user to delete their own review' do
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'KFC'
+    click_link 'Delete Review'
+
+    expect(page).not_to have_content('so so')
+    expect(current_path).to eq "/restaurants/#{kfc.id}"
+  end
+
+  scenario "does not allow user to delete another user's review" do
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Sign out'
+    sign_up('different@somewhere.com', '123456')
+    click_link 'KFC'
+    expect(page).not_to have_content 'Delete Review'
   end
 
 
