8b78fdda99dbaa88e0798bd67403c413e0375418
JulieLev <jkgee1@yahoo.co.uk>
2016-08-26 10:40:31 +0100

    fixes user unit test and restaurant index view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,19 +5,19 @@ describe User, type: :model do
 
   it { is_expected.to have_many :reviewed_restaurants }
 
-  # describe 'restaurants' do
-  #   let(:user) { User.create email: 'test@test.com' }
-  #   let(:restaurant_params) { {name: 'Diner'} }
-  #
-  #   subject(:restaurant) {
-  #  user.restaurants.create_with_user(restaurant_params, user) }
-  #
-  #   it 'creates a restaurant' do
-  #     expect(restaurant).to be_a Restaurant
-  #   end
-  #
-  #   it 'creates a restaurant associated with the specific user' do
-  #     expect(restaurant.user).to eq user
-  #   end
-  # end
+  describe 'restaurants' do
+    let(:user) { User.create email: 'test@test.com', password: '123456', password_confirmation: '123456' }
+    let(:restaurant_params) { {name: 'Diner'} }
+
+    subject(:restaurant) {
+    user.restaurants.create_with_user(restaurant_params, user) }
+
+    it 'creates a restaurant' do
+      expect(restaurant).to be_a Restaurant
+    end
+
+    it 'creates a restaurant associated with the specific user' do
+      expect(restaurant.user).to eq user
+    end
+  end
 end
